133e1c5a6c93ad45b3014368d0acbc2a4b427a02
sidhartha <sidharthakumaras@gmail.com>
2024-04-18 20:34:50 +0530

    mysql-setup

---
+++ mysql-db-setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+DATE=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f 1) # removing .sh in file name
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USER=$(id -u)
+
+if [ $USER -ne 0 ]
+then 
+    echo -e " $R Be a root user to install any package... $N"
+    exit 1 # manually stop without continue
+else 
+    echo "Root user"
+fi
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then    
+        echo -e "$2 is $R failed...$N" 
+        exit 1
+    else 
+        echo -e "$2 is $G Success...$N"
+    fi
+}
+
+
+dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "MYSQL Installation"
+
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "Enabling Mysql server"
+
+systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "Starting Mysql server"
+
+systemctl status mysqld &>>$LOGFILE
+VALIDATE $? "Startus of Mysql server"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+VALIDATE $? "Setting up root pwd for Mysql server"
+
+
