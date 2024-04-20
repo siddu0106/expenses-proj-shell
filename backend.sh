26ba33157791694b3f64c1c2fcd8d031fb320a05
sidhartha <sidharthakumaras@gmail.com>
2024-04-20 18:42:52 +0530

    backend-setup

--- backend.sh
+++ backend.sh
@@ -81,8 +81,7 @@ VALIDATE $? "Unzipping backend code to app folder"
 npm install &>>$LOGFILE
 VALIDATE $? "Installing node.js dependencies"
 
-#cp /home/ec2-user/expenses-proj-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
-cp /home/ec2-user/expenses-proj-shell/backend.service /tmp
+cp /home/ec2-user/expenses-proj-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
 VALIDATE $? "Copied backend service to systemd/system folder"
 
 systemctl daemon-reload &>>$LOGFILE
